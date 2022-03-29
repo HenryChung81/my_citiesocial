24e1af589852129836bf138b5946f38a823ff446
Henry Chung <chungjenhao81@gmail.com>
2022-03-29 13:29:56 +0800

    feat: add flash template

--- app/frontend/scripts/shared/application.js
+++ app/frontend/scripts/shared/application.js
@@ -21,3 +21,15 @@ document.addEventListener('turbolinks:load', () => {
     })
   }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  ;(document.querySelectorAll('.notification .delete') || []).forEach(
+    ($delete) => {
+      const $notification = $delete.parentNode
+
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification)
+      })
+    }
+  )
+})
