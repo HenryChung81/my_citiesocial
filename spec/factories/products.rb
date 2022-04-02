d7aeb1035bf48ef5bf161a0fd6592bd77c91ce6a
Henry Chung <chungjenhao81@gmail.com>
2022-04-02 18:01:49 +0800

    feat: add Product model

---
+++ spec/factories/products.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    vendor { nil }
+    list_price { "9.99" }
+    sell_price { "9.99" }
+    on_sell { false }
+    code { "MyString" }
+    deleted_at { "2022-04-02 17:34:39" }
+  end
+end
