46415008252c757791be6cb45d51baeef2667392
Henry Chung <chungjenhao81@gmail.com>
2022-04-12 13:29:48 +0800

    refactor: cart_spec.rb

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :product do
-    name { "MyString" }
-    vendor { nil }
-    list_price { "9.99" }
-    sell_price { "9.99" }
+    name { Faker::Name.name }
+    list_price { Faker::Number.between(to: 10, from: 50) }
+    sell_price { Faker::Number.between(to: 1, from: 50) }
     on_sell { false }
-    code { "MyString" }
-    deleted_at { "2022-04-02 17:34:39" }
+
+    vendor
+    category
   end
 end
