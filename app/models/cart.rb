02613c5eb621a4a4555aed6c8279135e2505beee
Henry Chung <chungjenhao81@gmail.com>
2022-04-13 19:58:41 +0800

    feat: add cart_spec '也可以存放在 Session 的內容（Hash 格式），還原成購物車的內容' test

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,19 @@
 class Cart
   attr_reader :items
 
-  def initialize
-    @items = []
+  def initialize(items = [])
+    @items = items
+  end
+
+  def self.from_hash(hash = nil)
+    if hash && hash["items"]
+      items = hash["items"].map { |item| 
+        CartItem.new(item["product_id"], item["quantity"])  
+      }
+      Cart.new(items)
+    else
+      Cart.new
+    end
   end
 
   def serialize
